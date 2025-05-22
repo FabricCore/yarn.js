c6f1fec27126c38034c65b5673d705218c419966
siriusmart <siriusmart@tuta.io>
2025-05-22 17:41:39 +0100

    replaced getters with actual functions because i didnt know js getters are lazy and messes everything up

--- getters.js
+++ getters.js
@@ -1,29 +1,5 @@
-Object.defineProperty(yarn, "client", {
-    get: function () {
-        return yarnwrap.client.MinecraftClient.getInstance();
-    },
-});
-
-Object.defineProperty(yarn, "clientPlayer", {
-    get: function () {
-        return yarn.client.player();
-    },
-});
-
-Object.defineProperty(yarn, "player", {
-    get: function () {
-        return yarn.toPlayerEntity(yarn.clientPlayer);
-    },
-});
-
-Object.defineProperty(yarn, "playerLiving", {
-    get: function () {
-        return yarn.toLivingEntity(yarn.clientPlayer);
-    },
-});
-
-Object.defineProperty(yarn, "playerEntity", {
-    get: function () {
-        return yarn.toEntity(yarn.clientPlayer);
-    },
-});
+yarn.client = () => yarnwrap.client.MinecraftClient.getInstance();
+yarn.clientPlayer = () => yarn.client().player();
+yarn.player = () => yarn.toPlayerEntity(yarn.clientPlayer());
+yarn.playerLiving = () => yarn.toLivingEntity(yarn.clientPlayer());
+yarn.playerEntity = () => yarn.toEntity(yarn.clientPlayer());
